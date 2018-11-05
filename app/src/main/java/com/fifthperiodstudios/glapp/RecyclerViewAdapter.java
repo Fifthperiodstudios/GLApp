fb50095aa1af47b3331e2befb11b315d3161f29f
Nils <>
2018-11-05 21:47:29 +0100

    Ich habe an den Settings weitergearbeitet; Ist noch nicht fertig.

--- app/src/main/java/com/fifthperiodstudios/glapp/RecyclerViewAdapter.java
+++ app/src/main/java/com/fifthperiodstudios/glapp/RecyclerViewAdapter.java
@@ -1,5 +1,6 @@
 package com.fifthperiodstudios.glapp;
 
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -8,24 +9,31 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.fifthperiodstudios.glapp.Login.MainActivity;
+
 class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolderKlasse> {
+
     @NonNull
     @Override
     public ViewHolderKlasse onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
         View einFach = LayoutInflater.from(parent.getContext()).inflate(R.layout.color_settings_element,null);
-        return null;
+
+        return new ViewHolderKlasse(einFach);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolderKlasse holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolderKlasse holder, final int position) {
 
+        holder.fachName.setText("Fachname aus ArrayListe");//Hier die Fächerliste einzeln aufrufen
+        holder.farbeWaehlen.setBackgroundColor(Color.BLUE);//Farbe der Einzelnen Fächer anzeigen/ eine zufällige Farbe erzeugen/nehmen, wenn es keine gibt.
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+
+        return 0;//Arraylist.getSize() länge der Liste ausgeben lassen
     }
 
     public class ViewHolderKlasse extends RecyclerView.ViewHolder{
