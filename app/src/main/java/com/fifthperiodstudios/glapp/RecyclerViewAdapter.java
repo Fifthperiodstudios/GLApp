7315d82ea310c6653ce6c97bf4b197ff1caad9f0
Nils <>
2018-11-08 22:02:47 +0100

    Der RecyclerView der Settings zeigt die Elemente jetzt richtig an

--- app/src/main/java/com/fifthperiodstudios/glapp/RecyclerViewAdapter.java
+++ app/src/main/java/com/fifthperiodstudios/glapp/RecyclerViewAdapter.java
@@ -1,6 +1,10 @@
 package com.fifthperiodstudios.glapp;
 
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.ShapeDrawable;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -24,13 +28,22 @@ class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewH
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolderKlasse holder, final int position) {
+    public void onBindViewHolder(@NonNull final ViewHolderKlasse holder, final int position) {
+
+        Drawable background = holder.farbeWaehlen.getBackground();
+        if (background instanceof ShapeDrawable) {
+            ((ShapeDrawable)background).getPaint().setColor(Color.BLUE/*.parseColor(stunde.getFach().getColor())*/);
+        } else if (background instanceof GradientDrawable) {
+            ((GradientDrawable)background).setColor(Color.GREEN/*.parseColor(stunde.getFach().getColor())*/);
+        } else if (background instanceof ColorDrawable) {
+            ((ColorDrawable)background).setColor(Color.RED/*.parseColor(stunde.getFach().getColor())*/);
+        }
 
         holder.fachName.setText("Fachname Nr:"+position);//Hier die Fächerliste einzeln aufrufen
-//        holder.farbeWaehlen.setBackgroundColor();//Farbe der Einzelnen Fächer anzeigen/ eine zufällige Farbe erzeugen/nehmen, wenn es keine gibt.
         holder.farbeWaehlen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+//                Toast.makeText(,"Das ist die Nachricht von Element: "+position,Toast.LENGTH_SHORT).show();
 //                zeige die Auswahl an
             }
         });
@@ -58,3 +71,65 @@ class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewH
         }
     }
 }
+/*
+public class StundenListAdapter extends RecyclerView.Adapter<StundenListAdapter.MyViewHolder> {
+    private StundenplanParser.Wochentag wochentag;
+
+    public StundenListAdapter(StundenplanParser.Wochentag w) {
+        wochentag = w;
+    }
+
+
+
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from (context);
+        View stundenView = inflater.inflate(R.layout.stundenlistelement, parent, false);
+        MyViewHolder viewHolder = new MyViewHolder (stundenView);
+        return viewHolder;
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Stunde stunde = wochentag.getStunden().get(position);
+        TextView kursnameView = holder.kursname;
+        TextView lehrerView = holder.lehrer;
+        TextView raumView = holder.raum;
+        TextView symbolView = holder.symbol;
+
+        Drawable background = symbolView.getBackground();
+        if (background instanceof ShapeDrawable) {
+            ((ShapeDrawable)background).getPaint().setColor(Color.parseColor(stunde.getFach().getColor()));
+        } else if (background instanceof GradientDrawable) {
+            ((GradientDrawable)background).setColor(Color.parseColor(stunde.getFach().getColor()));
+        } else if (background instanceof ColorDrawable) {
+            ((ColorDrawable)background).setColor(Color.parseColor(stunde.getFach().getColor()));
+        }
+        symbolView.setText(stunde.getFach().getKurs().contains("LK") ? stunde.getFach().getFach() + "LK" : stunde.getFach().getFach());
+        kursnameView.setText(stunde.getStunde() + ". " + stunde.getFach().getKurs());
+        lehrerView.setText(stunde.getFach().getLehrer());
+        raumView.setText(stunde.getFach().getRaum());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return wochentag.getStunden().size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView kursname;
+        public TextView lehrer;
+        public TextView raum;
+        public TextView symbol;
+        public MyViewHolder (View stundenView) {
+            super (stundenView);
+            kursname = (TextView) stundenView.findViewById(R.id.kursname);
+            lehrer = (TextView) stundenView.findViewById(R.id.lehrer);
+            raum = (TextView) stundenView.findViewById(R.id.raum);
+            symbol = (TextView) stundenView.findViewById(R.id.list_item_icon);
+        }
+    }
+}*/
